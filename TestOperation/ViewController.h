d1580133f4070ca279a2b492cbfa357b188e4b7c
acqiang <qiang@qiang-mbp.local>
2016-03-10 12:06:33 +0800

    Initial Commit

---
+++ TestOperation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestOperation
+//
+//  Created by qiang on 16/3/10.
+//  Copyright © 2016年 acqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestOperation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestOperation
+//
+//  Created by qiang on 16/3/10.
+//  Copyright © 2016年 acqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
